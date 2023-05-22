d99fed72a3e852b50b4c7b1ebdf989deae72e7a3
Yannik Kopyciok <ykpyck@eduroam-141-23-198-216.wlan.tu-berlin.de>
2023-05-22 16:39:09 +0200

    baselineIlp 1.1: unit testing to be done

--- src/baselineIlp.py
+++ src/baselineIlp.py
@@ -13,16 +13,16 @@ import ilpUtils
 
 try:
     # Input 
-    #   network             -> use networkx to load topology as graph 
-    G = ilpUtils.read_network("tests/test1-graph.txt")
-    #   transactions        -> tuple(start, dest, amount)
-    T = ilpUtils.read_transactions("tests/test1-transactions.txt")
-    #   paths               -> networkx to find 
-    P = ilpUtils.read_paths(G, T)
-    # proportional routing fee
-    prop_fee = 0.01
-    base_fee = 1
-    c_tr = 0.9
+    G = ilpUtils.read_network("tests/test1-graph.txt")  # load network from a list of edges with respective capacity, base fee, and routing fee
+    T = ilpUtils.read_transactions("tests/test1-transactions.txt")  # loads transaction as tuples like: tuple(start, dest, amount)
+    P = ilpUtils.read_paths(G, T)   # finds all possible paths for every transaction using an nx function
+    prop_fee, base_fee = 0.01, 1    # sets prop and base fee (possible to be individually set later)
+    c_tr, transaction_percentage = 0.8, 1   # sets percentage for , 0: a least succ trx amount 1: least num of succ trxs
+    #   needed vars for ILP operations: 
+    row_cons_iterator = 0           # sets the currently respected constraint/ row in sparse matrix and in the rhs vector
+    val_dyn = []
+    row_dyn = []
+    col_dyn = []
 
     # Create a new model
     m = gp.Model("baselineIlp")
@@ -32,28 +32,7 @@ try:
     x = m.addMVar(shape=len(P), vtype=GRB.BINARY, name="x")
 
     # Set objective                 -> routing_fee(t, p, ch_ij)
-    #   obj = np.array([1.0, 1.0, 2.0])
-    #   m.setObjective(obj @ x, GRB.MAXIMIZE)
-    obj = np.empty(shape=len(P), dtype=float)
-    index = 0
-    fee_dict = {}
-    for path in P:
-        trans_amount = int(T[path[1]][2])
-        routing_fee = float(0)
-        dest = None
-        for node in reversed(path[0]):
-            start = node
-            if dest == None:
-                dest = node
-            if dest != start and start != path[0][0]:
-                fee_dict.update({(start, dest, path[1], path[2]):trans_amount})
-                routing_fee = (trans_amount * prop_fee) + base_fee
-                trans_amount = trans_amount + routing_fee
-                dest = node
-            if start == path[0][0]:
-                fee_dict.update({(start, dest, path[1], path[2]):trans_amount})
-        obj[index] = routing_fee
-        index += 1
+    fee_dict, obj = ilpUtils.set_objective(P, T, prop_fee, base_fee)
     m.setObjective(obj @ x, GRB.MINIMIZE)
 
     # Build (sparse) constraint matrix 
@@ -61,74 +40,22 @@ try:
     #   loop over channels      -> checks capacity of all channels
     #       constraint matrix   -> per row relevant routing_fees and trans_amounts will be added 
     #       rhs vector          -> respective channel capacity
-    row_cons_iterator = 0
-    val_dyn = []
-    row_dyn = []
-    col_dyn = []
-    rhs = np.empty(shape=(len(G.edges)+1+len(T)), dtype=float)
-    rhs_index = 0
-    for edge in G.edges:                                                            # iteration over all edges (constraints)
-        col_path_iterator = 0
-        for path in P:                                                              # iteration over the paths (variables)
-            if ilpUtils.edge_in_path(edge, path[0]):                                # check if path is going over the respective edge
-                try:
-                    val_dyn.append(-fee_dict[(edge[0], edge[1], path[1], path[2])])
-                    #print(edge, " IS in: ", path, "added: ", fee_dict[(edge[0], edge[1], path[1], path[2])])
-                except:
-                    val_dyn.append(-fee_dict[(edge[1], edge[0], path[1], path[2])])
-                    #print(edge, " IS in: ", path, "added: ", fee_dict[(edge[1], edge[0], path[1], path[2])])
-                row_dyn.append(row_cons_iterator)                                   # if entry add row and col (sparse constraint matrix)
-                col_dyn.append(col_path_iterator)
-            #else:
-                #print(edge, " not in: ", path)
-            col_path_iterator += 1                                                  
-        rhs[rhs_index] = -G.edges[edge]["capacity"]
-        rhs_index += 1
-        row_cons_iterator += 1
-    #print(val_dyn)
-    #print(row_dyn)
-    #print(col_dyn)
-    #print(rhs)
-
+    val_dyn, row_dyn, col_dyn, row_cons_iterator, rhs = ilpUtils.capacity_constraints(G, T, P, fee_dict, val_dyn, row_dyn, col_dyn, row_cons_iterator)
 
     #   + 1                     -> percentage of successful transaction amounts
     #       constraint          -> all trans_amounts
     #       rhs                 -> c_tr times total trans_amount
-    col_path_iterator = 0
-    for path in P:
-        val_dyn.append(int(T[path[1]][2]))
-        row_dyn.append(row_cons_iterator)
-        col_dyn.append(col_path_iterator)
-        col_path_iterator += 1
-    rhs[rhs_index] = c_tr * ilpUtils.sum_of_T(T)
-    rhs_index += 1
-    row_cons_iterator += 1
-
     #   + 1*                    -> percentage of successful transactions
     #       constraint          -> all paths
     #       rhs                 -> c_tr times number of transactions
-
+    val_dyn, row_dyn, col_dyn, row_cons_iterator, rhs = ilpUtils.transaction_constraint(T, P, val_dyn, row_dyn, col_dyn, row_cons_iterator, rhs, c_tr, transaction_percentage)
     #   loop over transactions  -> checks that transactions are uniquely successful
     #       constraint          -> per row all paths of relevant transactions 
     #       rhs                 -> element of {0,1}
-    for t in T:
-        for path in P:
-            col_path_iterator = 0
-            val_dyn.append(-1)
-            row_dyn.append(row_cons_iterator)
-            col_dyn.append(col_path_iterator)
-            col_path_iterator += 1
-        rhs[rhs_index] = -1
-        rhs_index += 1
-        row_cons_iterator += 1
-    
-    # bring efficient python arrays to numpy arrays for further handling
-    val = np.array(val_dyn)
-    row = np.array(row_dyn)
-    col = np.array(col_dyn) 
+    val_dyn, row_dyn, col_dyn, row_cons_iterator, rhs = ilpUtils.transaction_uniqueness(T, P, val_dyn, row_dyn, col_dyn, row_cons_iterator, rhs)
 
     # build A sparse matrix
-    A = sp.csr_matrix((val, (row, col)), shape=(len(rhs), len(P)))
+    A = sp.csr_matrix((np.array(val_dyn), (np.array(row_dyn), np.array(col_dyn))), shape=(len(rhs), len(P)))
 
     # Add constraints
     m.addConstr(A @ x >= rhs, name="c")
@@ -137,20 +64,21 @@ try:
     m.optimize()
 
     # Display output 
-
     print(x.X)
     print('Obj: %g' % m.ObjVal)
-
+    plt.subplot(2, int(sum(x.X)), 1)
+    nx.drawing.nx_pylab.draw(G, with_labels=True)
     index = 0
+    plot_index = 1
     for val in x.X:
         if val != 0:
             highlighted_path = P[index][0]
             edge_colors = ['red' if edge in zip(highlighted_path[:-1], highlighted_path[1:]) else 'blue' for edge in G.edges]
-            plt.subplot(1, int(sum(x.X)), index)
+            plt.subplot(2, int(sum(x.X)), int(sum(x.X))+plot_index)
+            plot_index += 1
             nx.drawing.nx_pylab.draw(G, with_labels=True, edge_color=edge_colors)
         index += 1
     plt.show()
-    
 
 except gp.GurobiError as e:
     print('Error code ' + str(e.errno) + ": " + str(e))
--- src/ilpUtils.py
+++ src/ilpUtils.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3.9.6
 
 import networkx as nx
+import numpy as np
 
 def read_network(file_name: str):
     G = nx.readwrite.edgelist.read_edgelist(file_name, data=(("capacity", int),("routing_fee_base", float),("routing_fee_prop", float),))
@@ -39,3 +40,81 @@ def sum_of_T(T) -> float:
     for t in T:
         sum = sum + int(t[2])
     return sum
+
+def set_objective(P, T, prop_fee=0.1, base_fee=1):
+    obj = np.empty(shape=len(P), dtype=float)
+    index = 0
+    fee_dict = {}
+    for path in P:
+        trans_amount = int(T[path[1]][2])
+        routing_fee = float(0)
+        dest = None
+        for node in reversed(path[0]):
+            start = node
+            if dest == None:
+                dest = node
+            if dest != start and start != path[0][0]:
+                fee_dict.update({(start, dest, path[1], path[2]):trans_amount})
+                routing_fee = (trans_amount * prop_fee) + base_fee
+                trans_amount = trans_amount + routing_fee
+                dest = node
+            if start == path[0][0]:
+                fee_dict.update({(start, dest, path[1], path[2]):trans_amount})
+        obj[index] = routing_fee
+        index += 1
+    return fee_dict, obj
+
+def capacity_constraints(G, T, P, fee_dict, val_dyn, row_dyn, col_dyn, row_cons_iterator):
+    rhs = np.empty(shape=(len(G.edges)+1+len(T)), dtype=float)
+    for edge in G.edges:                                                            # iteration over all edges (constraints)
+        col_path_iterator = 0
+        for path in P:                                                              # iteration over the paths (variables)
+            if edge_in_path(edge, path[0]):                                # check if path is going over the respective edge
+                try:
+                    val_dyn.append(-fee_dict[(edge[0], edge[1], path[1], path[2])])
+                    #print(edge, " IS in: ", path, "added: ", fee_dict[(edge[0], edge[1], path[1], path[2])])
+                except:
+                    val_dyn.append(-fee_dict[(edge[1], edge[0], path[1], path[2])])
+                    #print(edge, " IS in: ", path, "added: ", fee_dict[(edge[1], edge[0], path[1], path[2])])
+                row_dyn.append(row_cons_iterator)                                   # if entry add row and col (sparse constraint matrix)
+                col_dyn.append(col_path_iterator)
+            col_path_iterator += 1                                                  
+        rhs[row_cons_iterator] = -G.edges[edge]["capacity"]
+        row_cons_iterator += 1
+    return val_dyn, row_dyn, col_dyn, row_cons_iterator, rhs
+
+def transaction_constraint(T, P, val_dyn, row_dyn, col_dyn, row_cons_iterator, rhs, c_tr, transaction_percentage):
+    if transaction_percentage == 0:
+        col_path_iterator = 0
+        for path in P:
+            val_dyn.append(int(T[path[1]][2]))
+            row_dyn.append(row_cons_iterator)
+            col_dyn.append(col_path_iterator)
+            col_path_iterator += 1
+        rhs[row_cons_iterator] = c_tr * sum_of_T(T)
+        row_cons_iterator += 1
+    else:
+        col_path_iterator = 0
+        for path in P:
+            val_dyn.append(1)
+            row_dyn.append(row_cons_iterator)
+            col_dyn.append(col_path_iterator)
+            col_path_iterator += 1
+        rhs[row_cons_iterator] = c_tr * len(T)
+        row_cons_iterator += 1
+    return val_dyn, row_dyn, col_dyn, row_cons_iterator, rhs
+
+def transaction_uniqueness(T, P, val_dyn, row_dyn, col_dyn, row_cons_iterator, rhs):
+    t_index = 0
+    for t in T:
+        col_path_iterator = 0
+        for path in P:
+            if path[1] == t_index:
+                val_dyn.append(-1)
+                row_dyn.append(row_cons_iterator)
+                col_dyn.append(col_path_iterator)
+            col_path_iterator += 1
+        rhs[row_cons_iterator] = -1
+        row_cons_iterator += 1
+        t_index += 1
+    return val_dyn, row_dyn, col_dyn, row_cons_iterator, rhs
